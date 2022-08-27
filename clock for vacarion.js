c294dac67ece85a84db5fc3bc047249dde0c916a
Lina Veliar <veliar.helly@gmail.com>
2022-08-27 22:01:34 +0300

    codewars

---
+++ clock for vacarion.js
@@ -0,0 +1,24 @@
+function setAlarm(employed, vacation){
+
+  
+    if (employed == true && vacation == true){
+      return false
+    } 
+    else if (employed == false && vacation == true){
+      return false
+    }  
+    else if (employed == true && vacation == false){
+      return true
+    } 
+    else if (employed == false && vacation == false){
+      return false
+    } 
+    else {
+      return false
+    }
+    
+  }
+
+  function setAlarm(employed, vacation){
+    return (employed && !vacation) ? true : false;
+  }
